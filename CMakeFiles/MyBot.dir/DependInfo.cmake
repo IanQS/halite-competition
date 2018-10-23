94e7d2207656423d031c52933ccc52762a4a2806
Ian Q <itq@alumni.cmu.edu>
2018-10-23 08:59:41 -0400

    adding starter code

---
+++ CMakeFiles/MyBot.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/itq/Desktop/Projects/halite/MyBot.cpp" "/home/itq/Desktop/Projects/halite/CMakeFiles/MyBot.dir/MyBot.cpp.o"
+  "/home/itq/Desktop/Projects/halite/hlt/command.cpp" "/home/itq/Desktop/Projects/halite/CMakeFiles/MyBot.dir/hlt/command.cpp.o"
+  "/home/itq/Desktop/Projects/halite/hlt/constants.cpp" "/home/itq/Desktop/Projects/halite/CMakeFiles/MyBot.dir/hlt/constants.cpp.o"
+  "/home/itq/Desktop/Projects/halite/hlt/dropoff.cpp" "/home/itq/Desktop/Projects/halite/CMakeFiles/MyBot.dir/hlt/dropoff.cpp.o"
+  "/home/itq/Desktop/Projects/halite/hlt/game.cpp" "/home/itq/Desktop/Projects/halite/CMakeFiles/MyBot.dir/hlt/game.cpp.o"
+  "/home/itq/Desktop/Projects/halite/hlt/game_map.cpp" "/home/itq/Desktop/Projects/halite/CMakeFiles/MyBot.dir/hlt/game_map.cpp.o"
+  "/home/itq/Desktop/Projects/halite/hlt/log.cpp" "/home/itq/Desktop/Projects/halite/CMakeFiles/MyBot.dir/hlt/log.cpp.o"
+  "/home/itq/Desktop/Projects/halite/hlt/player.cpp" "/home/itq/Desktop/Projects/halite/CMakeFiles/MyBot.dir/hlt/player.cpp.o"
+  "/home/itq/Desktop/Projects/halite/hlt/ship.cpp" "/home/itq/Desktop/Projects/halite/CMakeFiles/MyBot.dir/hlt/ship.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "hlt"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
